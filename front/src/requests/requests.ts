e14fdb34cb0656468c204228f61540cd8aac11ea
Volodymyr Nerovnia <nerv@i.ua>
2024-04-30 23:45:05 +0300

    Front[requests]: Split mime-type and data in getQrCodeImage function

--- front/src/requests/requests.ts
+++ front/src/requests/requests.ts
@@ -4,7 +4,8 @@ export function getQrCodeImage() {
   return async () => {
     try {
       const response = await axios.get('http://localhost:3000/');
-      return response?.data.qrcode;
+      const base64img = response?.data.qrcode.split(',')[1];
+      return base64img;
     } catch (err) {
       console.log(err);
     }
