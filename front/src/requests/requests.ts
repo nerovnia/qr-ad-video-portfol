2bff71e31c9b5d0cd7b5af81216ad87383e891f1
Volodymyr Nerovnia <nerv@i.ua>
2024-05-01 03:50:45 +0300

    Front: Enhanced requests module

--- front/src/requests/requests.ts
+++ front/src/requests/requests.ts
@@ -1,10 +1,15 @@
 import axios from 'axios';
+import { ImageBase64 } from "../types/types";
 
-export function getQrCodeImage() {
+export function getQrCodeImage(): Promise<ImageBase64> {
   return async () => {
     try {
       const response = await axios.get('http://localhost:3000/');
-      const base64img = response?.data.qrcode.split(',')[1];
+      const separateTypeAndDataString = response?.data.qrcode.split(',');
+      const base64img: ImageBase64 = {
+        mtype: separateTypeAndDataString[0].slice(5,-7),
+        data: separateTypeAndDataString[1]
+      }
       return base64img;
     } catch (err) {
       console.log(err);
