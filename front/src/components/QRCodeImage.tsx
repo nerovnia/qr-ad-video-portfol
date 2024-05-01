fe2d5409c7e7af511e88e3085ea9ffb2c7322c34
Volodymyr Nerovnia <nerv@i.ua>
2024-05-01 03:49:41 +0300

    Front: Refactored QRCodeImage module

--- front/src/components/QRCodeImage.tsx
+++ front/src/components/QRCodeImage.tsx
@@ -1,30 +1,27 @@
 import { useState, useEffect } from "react";
 import { getQrCodeImage } from "../requests/requests";
-
-const decodeBase64 = (base64String: string): Uint8Array => {
-  const binaryString = atob(base64String);
-
-  const bytes = new Uint8Array(binaryString.length);
-  for (let i = 0; i < binaryString.length; i++) {
-    bytes[i] = binaryString.charCodeAt(i);
-  }
-  const blob = new Blob([bytes], { type: 'image/png' });
-  const imageUrl = URL.createObjectURL(blob);
-  return imageUrl;
-};
+import { decodeBase64 } from "../utils/converters";
+import { ImageBase64 } from "../types/types";
 
 export function QRCodeImage() {
 
-  const [image, setImage] = useState<Uint8Array>(new Uint8Array());
+
+
+  const [image, setImage] = useState<ImageBase64>({mtype: '', data: new Uint8Array()});
   useEffect(() => {
     getQrCodeImage()()
-    .then((image: string) => setImage(decodeBase64(image)))
-    .catch((error)=> console.log(error));
+    .then((image: ImageBase64) => setImage(
+      {
+        mtype: image.mtype, 
+        data: decodeBase64(image.data)
+      }
+    ))
+    .catch((error: string)=> console.log(error));
   }, []);
 
   return (
     <>
-      <img src={image} alt="QRCode" />
+      <img src={image.data  } alt="QRCode" />
     </>
   );
 }
